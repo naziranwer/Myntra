32e77a464ad513ce2a5c86c6196428d2b81137b1
Mohd Nazir Anwer <anwernazir674@gmail.com>
2023-08-11 00:28:21 +0530

    final commit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,33 +1,33 @@
 import React, { useState, useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
-import SearchIcon from "@material-ui/icons/Search";
+import { Paper, InputBase, IconButton } from "@mui/material";
+import { Search as SearchIcon } from "@mui/icons-material";
+
 import { useDispatch } from "react-redux";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    alignItems: "center",
-    width: 400,
-    height: 30,
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: theme.palette.common.white,
-    "&:hover": {
-      backgroundColor: theme.palette.grey[100],
-    },
-  },
-  input: {
-    marginLeft: theme.spacing(1),
-    flex: 1,
-  },
-  icon: {
-    padding: theme.spacing(1, 2),
-    color: theme.palette.grey[500],
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     display: "flex",
+//     alignItems: "center",
+//     width: 400,
+//     height: 30,
+//     borderRadius: theme.shape.borderRadius,
+//     backgroundColor: theme.palette.common.white,
+//     "&:hover": {
+//       backgroundColor: theme.palette.grey[100],
+//     },
+//   },
+//   input: {
+//     marginLeft: theme.spacing(1),
+//     flex: 1,
+//   },
+//   icon: {
+//     padding: theme.spacing(1, 2),
+//     color: theme.palette.grey[500],
+//   },
+// }));
 
 const SearchBar = () => {
-  const classes = useStyles();
+  // const classes = useStyles();
   const dispatch = useDispatch();
   const [searchTerm, setSearchTerm] = useState("");
 
@@ -42,15 +42,37 @@ const SearchBar = () => {
   console.log("search bar renders");
 
   return (
-    <div className={classes.root}>
-      <SearchIcon className={classes.icon} />
-      <TextField
+    // <div className={classes.root}>
+    //   <SearchIcon className={classes.icon} />
+    //   <TextField
+    //     onChange={handleInputChange}
+    //     className={classes.input}
+    //     placeholder="Search..."
+    //     // variant="outlined"
+    //   />
+    // </div>
+
+    <Paper
+      component="form"
+      sx={{
+        p: "2px 4px",
+        display: "flex",
+        alignItems: "center",
+        width: 400,
+        borderRadius: "999px",
+        boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+      }}
+    >
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search for products,brands and more"
+        inputProps={{ "aria-label": "search" }}
         onChange={handleInputChange}
-        className={classes.input}
-        placeholder="Search..."
-        // variant="outlined"
       />
-    </div>
+      <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+    </Paper>
   );
 };
 
